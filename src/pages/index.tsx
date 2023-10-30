f86b9fcff0bfac25ee8306579c037ac25e7237f2
myuto <m.yuto72319@gmail.com>
2023-10-30 23:40:33 +0900

    Add: shisha log list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,48 @@
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
 
 type PageProps = {
   data: ShishaLog[];
 };
 
-type ShishaLog = {
-  id: number;
-  title: string;
-};
-
-// const Page: NextPage<PageProps> = ({data}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-const Page: NextPage<PageProps> = () => {
-  const [data, setData] = useState<ShishaLog[]>(null!);
-
-  useEffect(() => {
-    fetch("/testData/shishaLog.json")
-      .then((response) => response.json())
-      .then((jsonData) => {
-        setData(jsonData);
-      })
-      .catch((error) => {
-        console.error("Error fetching JSON:", error);
-      });
-  }, []);
-
+const Page: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ data }) => {
   return (
-    data ? 
     <>
-      {data.map((v, i) => (
-        <Link key={i} href={`/${v.id}`}>
-          <LogCard id={v.id} title={v.title} />
-        </Link>
-      ))}
-    </>
-    :
-    <>
-      <p>記録がありません</p>
+      <ShishaLogList data={data} />
     </>
   );
 };
 export default Page;
 
+const ShishaLogList: React.FC<PageProps> = ({ data }) => {
+  return (
+    <>
+      {data ? (
+        data.map((v, i) => (
+          <Link key={i} href={`/${v.id}`}>
+            <LogCard id={v.id} title={v.title} />
+          </Link>
+        ))
+      ) : (
+        <p>記録がありません</p>
+      )}
+    </>
+  );
+};
+
 const LogCard: React.FC<ShishaLog> = ({ id, title }) => {
   return (
     <div>
-      <p>{title}</p>
+      <div>
+        <p>{title}</p>
+      </div>
     </div>
   );
 };
 
-// const getServerSideProps: GetServerSideProps<PageProps> = async () => {
-//   const res = await fetch("/testData/shishaLog.json");
-//   const data = await res.json();
+export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
+  const res = await fetch("http://localhost:3000/testData/shishaLog.json");
+  const data = await res.json();
 
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// };
+  return { props: { data } };
+};

d50923043fa3a03585d4f99eb1d40e1387031223
myuto <m.yuto72319@gmail.com>
2023-10-31 22:48:17 +0900

    Add: Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+export const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className="min-h-screen">
-      <Component {...pageProps}/>
-    </main>
-)
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }

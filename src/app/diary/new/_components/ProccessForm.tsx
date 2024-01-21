25f250c024b9ab86c4bd081104d0161fc5350331
Dz0526 <this@dz99.me>
2024-01-21 22:58:31 +0900

    feat: add form2 ui

--- src/app/diary/new/_components/ProccessForm.tsx
+++ src/app/diary/new/_components/ProccessForm.tsx
@@ -1,9 +1,26 @@
 import { Control, UseFormRegister } from "react-hook-form";
-import { Input } from "@mui/material";
+import { TextareaAutosize } from "@mui/material";
+import CustomHeading from "@/_components/customHeading";
 
 type ProcessFormProps = {
   register: UseFormRegister<DiaryFormValues>;
   control: Control<DiaryFormValues>;
 };
 
-export const ProcessForm: React.FC<ProcessFormProps> = ({ register, control }) => <Input {...register("process")} />;
+export const ProcessForm: React.FC<ProcessFormProps> = ({ register, control }) => (
+  <>
+    <CustomHeading>Sucking Out</CustomHeading>
+    <TextareaAutosize
+      minRows={10}
+      style={{
+        width: "100%",
+        backgroundColor: "#292929",
+        borderRadius: "5px",
+        borderColor: "#B6A2D1",
+        color: "#B6A2D1",
+        marginTop: "20px",
+      }}
+      {...register("process")}
+    />
+  </>
+);
